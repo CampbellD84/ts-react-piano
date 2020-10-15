e9681ffea431ef9d9274de5100ec3c5240eec5ee
Douglas Campbell <dacampbelltbone@gmail.com>
2020-10-15 02:30:05 -0400

    Create Render Props w/ Function Component and w/ Class.

--- src/adapters/Soundfont/SoundfontProvider.ts
+++ src/adapters/Soundfont/SoundfontProvider.ts
@@ -1,45 +1,53 @@
-import { useState, useRef } from "react"
-import Soundfont, { InstrumentName, Player } from "soundfont-player"
+import { FunctionComponent, ReactElement, useCallback, useEffect, useRef, useState } from "react";
+import Soundfont, { InstrumentName, Player } from "soundfont-player";
 import { MidiValue } from "../../domain/note";
 import { Optional } from "../../domain/types"
-import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "../../domain/sound"
+import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "../../domain/sound";
 
-
-interface Settings {
-    AudioContext: AudioContextType
-}
-
-interface Adapted {
+interface ProvidedProps {
     loading: boolean
-    current: Optional<InstrumentName>
-
-    load(instrument?: InstrumentName): Promise<void>
     play(note: MidiValue): Promise<void>
     stop(note: MidiValue): Promise<void>
 }
 
-export function useSoundfont({ AudioContext }: Settings): Adapted {
+interface ProviderProps {
+    instrument?: InstrumentName
+    AudioContext: AudioContextType
+    render(props: ProvidedProps): ReactElement
+}
+
+export const SoundfontProvider: FunctionComponent<ProviderProps> = ({
+    AudioContext,
+    instrument,
+    render
+}) => {
+
     let activeNodes: AudioNodesRegistry = {}
+
     const [current, setCurrent] = useState<Optional<InstrumentName>>(null)
     const [loading, setLoading] = useState<boolean>(false)
     const [player, setPlayer] = useState<Optional<Player>>(null)
     const audio = useRef(new AudioContext())
 
+    const loadInstrument = useCallback(() => load(instrument), [instrument])
+
+    useEffect(() => {
+        if (!loading && instrument !== current) loadInstrument()
+    }, [loadInstrument, loading, instrument, current])
+
+    async function resume() {
+        return audio.current.state === "suspended"
+            ? await audio.current.resume()
+            : Promise.resolve()
+    }
+
     async function load(instrument: InstrumentName = DEFAULT_INSTRUMENT) {
         setLoading(true)
-        const player = await Soundfont.instrument(
-            audio.current,
-            instrument
-        )
+        const player = await Soundfont.instrument(audio.current, instrument)
 
         setLoading(false)
         setCurrent(instrument)
         setPlayer(player)
-    }
-
-    async function resume() {
-        return audio.current.state === "suspended"
-            ? await audio.current.resume() : Promise.resolve()
     }
 
     async function play(note: MidiValue) {
@@ -58,11 +66,9 @@ export function useSoundfont({ AudioContext }: Settings): Adapted {
         activeNodes = { ...activeNodes, [note]: null }
     }
 
-    return {
+    return render({
         loading,
-        current,
-        load,
         play,
         stop
-    }
+    })
 }
--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useEffect } from "react"
+import React, { FunctionComponent } from "react"
 import { useAudioContext } from "../AudioContextProvider"
-import { useSoundfont } from "../../adapters/Soundfont"
+import { SoundfontProvider } from "../../adapters/Soundfont"
 import { Keyboard } from "../Keyboard"
 import { useInstrument } from "../../state/Instrument"
 import "./style.css"
@@ -9,11 +9,13 @@ import "./style.css"
 export const KeyboardWithInstrument: FunctionComponent = () => {
     const AudioContext = useAudioContext()!
     const { instrument } = useInstrument()
-    const { loading, current, play, stop, load } = useSoundfont({ AudioContext })
 
-    useEffect(() => {
-        if (!loading && instrument !== current) load(instrument)
-    }, [load, loading, current, instrument])
 
-    return <Keyboard loading={loading} play={play} stop={stop} />
+    return (
+        <SoundfontProvider
+            AudioContext={AudioContext}
+            instrument={instrument}
+            render={(props) => <Keyboard {...props} />}
+        />
+    )
 }
